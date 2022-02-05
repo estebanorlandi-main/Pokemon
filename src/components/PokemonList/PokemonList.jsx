bda32cb04aca32aa8897ec13adcb31878de9ef19
estebanorlandi4 <estebanorlandi4@gmail.com>
2022-02-04 23:05:04 -0300

    feat: PokemonList component

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -1,4 +1,30 @@
+const colors = {
+  grass: "#78c850",
+  fire: "#f08030",
+  water: "#6890f0",
+  bug: "#a8b820",
+  normal: "#a8a878",
+  poison: "#a040a0",
+  electric: "#f8d030",
+  ground: "#e0c068",
+  fairy: "#ee99ac",
+  fighting: "#c03028",
+  psychic: "#f85888",
+  rock: "#b8a038",
+  ghost: "#705898",
+  ice: "#98d8d8",
+  dragon: "#7038f8",
+};
+
 function PokemonList() {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    getPokemons().then((res) => {
+      setPokemons(res.map(({ data }) => data));
+    });
+  }, []);
+
   return (
     <div>
       {pokemons.map(({ id, name, type }) => (
@@ -6,8 +32,18 @@ function PokemonList() {
           <img
             src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`}
           />
+          <p>{name}</p>
+          <ul>
+            {types.map(({ type: { name } }, i) => (
+              <li key={name + i} style={{ backgroundColor: colors[name] }}>
+                {name}
+              </li>
+            ))}
+          </ul>
         </div>
       ))}
     </div>
   );
 }
+
+export default PokemonList;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import PokemonList from "../../components/PokemonList/PokemonList";
+
+function Home() {
+  return (
+    <main>
+      <PokemonList />
+    </main>
+  );
+}
+
+export default Home;
