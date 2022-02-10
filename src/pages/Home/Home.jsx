c1d055949a1f5e948a87ce99c1b842477d94e6e1
estebanorlandi4 <estebanorlandi4@gmail.com>
2022-02-10 20:44:06 -0300

    fix: landing padding

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { BiChevronRight, BiChevronLeft } from "react-icons/bi";
 
@@ -6,22 +6,24 @@ import { fetchPokemons, removePokemons } from "redux/actions/pokemon";
 
 import PrimaryButton from "components/Buttons/PrimaryButton";
 import PokemonList from "components/PokemonList/PokemonList";
-//import { useQuery } from "utils/hooks";
 
 import styles from "./Home.module.css";
 
 export function Home() {
   const dispatch = useDispatch();
-  //const query = useQuery();
+
+  const [page, setPage] = useState(1);
 
   useEffect(() => dispatch(fetchPokemons()), [dispatch]);
 
   const { prev, next } = useSelector((state) => state.pokemons);
   const prevPage = () => {
+    setPage(page - 1);
     dispatch(removePokemons());
     dispatch(fetchPokemons(prev));
   };
   const nextPage = () => {
+    setPage(page + 1);
     dispatch(removePokemons());
     dispatch(fetchPokemons(next));
   };
@@ -30,12 +32,9 @@ export function Home() {
     <main>
       <div className={styles.container}>
         <div className={styles.pageHandler}>
-          {prev ? (
-            <PrimaryButton Icon={BiChevronLeft} onClick={prevPage} />
-          ) : null}
-          {next ? (
-            <PrimaryButton Icon={BiChevronRight} onClick={nextPage} />
-          ) : null}
+          <PrimaryButton Icon={BiChevronLeft} onClick={prevPage} />
+          {page}
+          <PrimaryButton Icon={BiChevronRight} onClick={nextPage} />
         </div>
 
         <PokemonList />
