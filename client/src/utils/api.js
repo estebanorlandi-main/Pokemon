7c9f6a3d6af96ac2522984cab33acca9b6ff7ef2
estebanorlandi4 <estebanorlandi4@gmail.com>
2022-02-17 21:33:13 -0300

    .

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,10 +3,10 @@ import axios from "axios";
 const { REACT_APP_API_URL } = process.env;
 
 export const getPokemons = async (page, type) => {
-  const typeQuery = type ? "?type=" + type : "";
-  console.log(type);
+  const typeQuery = type ? `?type=${type}` : "";
 
   const url = REACT_APP_API_URL + `/pokemons${typeQuery}`;
+
   const { data } = await axios(url);
   return data;
 };
