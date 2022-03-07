6393db819244ea1ef73afa6e9b68fb510823b995
estebanorlandi4 <estebanorlandi4@gmail.com>
2022-03-07 18:17:02 -0300

    feat: reload db

--- api/src/index.ts
+++ api/src/index.ts
@@ -16,7 +16,7 @@ if (NODE_ENV !== "production") {
 app.use(cors());
 app.use(routes);
 
-run({ reload: false });
+run({ reload: true });
 
 app.listen(PORT, () =>
   console.log(`[server]: Server is running at port ${PORT}`)
